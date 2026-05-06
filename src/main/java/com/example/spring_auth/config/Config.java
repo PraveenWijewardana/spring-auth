af3c1b55d0c69ca672fc9da5a5084e607a209e84
praveenheshanawijewardana <praveenwijewardana1@gmail.com>
2026-05-06 10:53:01 +0530

    add in-memory user details service with password encoding

--- src/main/java/com/example/spring_auth/config/Config.java
+++ src/main/java/com/example/spring_auth/config/Config.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +27,27 @@ public class Config {
         .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService bean(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("nimal")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
